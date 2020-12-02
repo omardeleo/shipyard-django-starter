98e9acf6b7a25534e3a92ce9c2f5b1c6769ad1d5
Omar De Leo <omar.deleo@gmail.com>
2020-12-02 17:11:19 -0500

    Create and migrate Counter model

--- counters/views.py
+++ counters/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from .models import Counter
 
 def index(request):
-    return HttpResponse("This is the counter!")
+    counter = Counter.objects.last()
+    if counter:
+      counter.value = counter.value + 1
+      counter.save()
+    else:
+      counter = Counter()
+      counter.save()
+    return HttpResponse(counter.value)
