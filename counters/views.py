86edf967d03b0f5e772b9561580ea5560247b1d5
Omar De Leo <omar.deleo@gmail.com>
2020-12-03 11:49:22 -0500

    Fix indentation

--- counters/views.py
+++ counters/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 from .models import Counter
 
 def index(request):
     counter = Counter.objects.last()
     if counter:
-      counter.value = counter.value + 1
-      counter.save()
+        counter.value = counter.value + 1
+        counter.save()
     else:
-      counter = Counter()
-      counter.save()
+        counter = Counter()
+        counter.save()
     return HttpResponse(counter.value)
