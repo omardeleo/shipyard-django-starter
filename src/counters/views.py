95c294bc88a365993ac90cb42ed0ff4b8b83dba2
Omar De Leo <omar.deleo@gmail.com>
2020-12-04 16:26:09 -0500

    Refactor counter

--- src/counters/views.py
+++ src/counters/views.py
@@ -6,10 +6,9 @@ from .models import Counter
 def index(request):
     counter = Counter.objects.last()
     if counter:
-        counter.value = counter.value + 1
-        counter.save()
+        counter.increment()
     else:
         counter = Counter()
         counter.save()
-    context = {'counter': counter.value}
+    context = {'count': counter.value}
     return render(request, 'counters/index.html', context)
